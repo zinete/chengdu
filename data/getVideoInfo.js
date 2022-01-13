f6abc502f24f137ebc797d2c7ff56e075ef5aa48
zhenghui <zhenghui@zinete.com>
2022-01-13 19:09:38 +0800

    init

---
+++ data/getVideoInfo.js
@@ -0,0 +1,75 @@
+
+export default {
+
+  data: [
+    {
+      lang: "zh",
+      bg: "../bg/img_top_bg_cn.png",
+      video: [
+
+        {
+          cityIcon: "../cn/chengdu_cn.png",
+          cover: "../cn/cn01.png",
+          key: 2,
+          local: "../cn/cn01.mp4",
+          title: "d1",
+          lang: "title1",
+          panda: "../bg/panda1.png",
+        },
+        {
+          cityIcon: "../cn/zigong_cn.png",
+          cover: "../cn/cn02.png",
+          key: 3,
+          title: "d2",
+          local: "../cn/cn02.mp4",
+          lang: "title2",
+          panda: "../bg/panda2.png",
+        },
+        {
+          cityIcon: "../cn/langzhong_cn.png",
+          cover: "../cn/cn03.png",
+          title: "d3",
+          key: 4,
+          local: "../cn/cn03.mp4",
+          lang: "title3",
+          panda: "../bg/panda3.png",
+        },
+
+      ],
+    },
+    {
+      lang: "en",
+      bg: "../bg/img_top_bg_cn.png",
+      video: [
+        {
+          cityIcon: "../en/chengdu_en.png",
+          cover: "../en/en01.png",
+          key: 2,
+          title: "d1",
+          lang: "title1",
+          local: "../en/en01.mp4",
+          panda: "../bg/panda1.png",
+        },
+        {
+          cityIcon: "../en/zigong_en.png",
+          cover: "../en/en02.png",
+          key: 3,
+          title: "d2",
+          lang: "title2",
+          local: "../en/en02.mp4",
+          panda: "../bg/panda2.png",
+        },
+        {
+          cityIcon: "../en/langzhong_en.png",
+          cover: "../en/en03.png",
+          key: 4,
+          title: "d3",
+          lang: "title3",
+          local: "../en/en03.mp4",
+          panda: "../bg/panda3.png",
+        },
+
+      ],
+    },
+  ],
+};
---
+++ locales/en-US.js
@@ -0,0 +1,10 @@
+export default {
+  lang: "cn",
+  title: "匠心",
+  d1: "Chengdu—A City of Gastronomy",
+  d2: "Zigong—Distinctive Cuisine of the Millennia-old Salt Capital ",
+  d3: "Langzhong—Zhang Fei Beef Jerky from the Birthplace of Chinese New Year Culture",
+  title1: "      Zigong takes pride in its 2,000-year-old salt mining culture. Zigong well salt is precious ingredient for local cuisine and has fostered Yanbang cuisine characterized by heavy taste and diverse flavors. Zigong is also known for lantern shows, which create fantastic visual feasts for global viewers. This is Zigong, a city noted for salt and multiple flavors. ",
+  title2: "      Zigong takes pride in its 2,000-year-old salt mining culture. Zigong well salt is precious ingredient for local cuisine and has fostered Yanbang cuisine characterized by heavy taste and diverse flavors. Zigong is also known for lantern shows, which create fantastic visual feasts for global viewers. This is Zigong, a city noted for salt and multiple flavors. ",
+  title3: "Langzhong is hailed as the `Birthplace of Spring Festival Culture` in China. It is embraced by the wandering Jialing River, creating a beautiful painting of the city in harmony with mountains and rivers. As Spring Festival draws near, you’re welcome to Langzhong and experience a very happy, healthy and prosperous Chinese New Year. "
+};
